9874357ceed57528c31cb6aacf19a8e7b3e01055
lutfiyet <ltfytpcdrn@gmail.com>
2020-12-09 01:09:10 +0100

    ins bitti

--- src/test/java/FeatureFiles/_US_03_Fees.feature
+++ src/test/java/FeatureFiles/_US_03_Fees.feature
@@ -5,13 +5,16 @@ Feature: Fees Create Edit Delete Functionality
     When Enter username and password and click Login button
 
   Scenario: Create a Fee Type
+    Then go to Fees Page
     When Create a Fee Type name as "fee4" and code as "code3" and priority as "1"
     Then Success message should be displayed
 
   Scenario: Edit a Fee Type
+    Then go to Fees Page
     When User edit the "fee4" to "fee8"
     Then Success message should be displayed
 
   Scenario: Delete a Fee Type
+    Then go to Fees Page
     When User delete the "fee8"
     Then Success message should be displayed
--- src/test/java/StepDefinitions/_US_03_FeeSteps.java
+++ src/test/java/StepDefinitions/_US_03_FeeSteps.java
@@ -1,31 +1,55 @@
 package StepDefinitions;
 
+import Pages._US_03_DialogContent;
+import Pages._US_03_LeftNavigation;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class _US_03_FeeSteps {
+    _US_03_DialogContent dialogContent3 = new _US_03_DialogContent();
+    _US_03_LeftNavigation leftNavigation3 = new _US_03_LeftNavigation();
 
-        @When("^Create a Fee Type name as \"([^\"]*)\" and code as \"([^\"]*)\" and priority as \"([^\"]*)\"$")
-        public void createAFeeTypeNameAsAndCodeAsAndPriorityAs(String arg0, String arg1, String arg2) throws Throwable {
-            // Write code here that turns the phrase above into concrete actions
-            throw new PendingException();
-        }
-
-        @Then("^Success message should be displayed$")
-        public void successMessageShouldBeDisplayed() {
-        }
-
-        @When("^User edit the \"([^\"]*)\" to \"([^\"]*)\"$")
-        public void userEditTheTo(String arg0, String arg1) throws Throwable {
-            // Write code here that turns the phrase above into concrete actions
-            throw new PendingException();
-        }
-
-        @When("^User delete the \"([^\"]*)\"$")
-        public void userDeleteThe(String arg0) throws Throwable {
-            // Write code here that turns the phrase above into concrete actions
-            throw new PendingException();
-        }
+    @Then("^go to Fees Page$")
+    public void goToFeesPage() {
+        leftNavigation3.findElementAndClickFunction("setupOne");
+        leftNavigation3.findElementAndClickFunction("parameters");
+        leftNavigation3.findElementAndClickFunction("fees");
     }
+
+    @When("^Create a Fee Type name as \"([^\"]*)\" and code as \"([^\"]*)\" and priority as \"([^\"]*)\"$")
+    public void createAFeeTypeNameAsAndCodeAsAndPriorityAs(String name, String code, String priority) {
+        dialogContent3.findElementAndClickFunction("addfeetypebutton");
+        dialogContent3.findElementAndSendKeysFunction("nameInput", name);
+        dialogContent3.findElementAndSendKeysFunction("codeInput", code);
+        dialogContent3.findElementAndSendKeysFunction("priorityInput", priority);
+        dialogContent3.findElementAndClickFunction("saveButton");
+    }
+
+    @Then("^Success message should be displayed$")
+    public void successMessageShouldBeDisplayed() {
+        dialogContent3.findElementAndVerifyContainsText("msjContainer", "successfully");
+    }
+
+    @When("^User edit the \"([^\"]*)\" to \"([^\"]*)\"$")
+    public void userEditTheTo(String oldfee, String newfee) {
+        dialogContent3.findElementAndSendKeysFunction("SearchNameInput", oldfee);
+        dialogContent3.findElementAndClickFunction("SearchGeneral");
+        dialogContent3.findElementAndEditFunction(oldfee);
+        dialogContent3.findElementAndSendKeysFunction("nameInput", newfee);
+        dialogContent3.findElementAndClickFunction("saveButton");
+
+    }
+
+    @When("^User delete the \"([^\"]*)\"$")
+    public void userDeleteThe(String lastfee)  {
+        dialogContent3.findElementAndSendKeysFunction("SearchNameInput",lastfee);
+        dialogContent3.findElementAndClickFunction("SearchGeneral");
+        dialogContent3.findElementAndDeleteFunction(lastfee);
+       dialogContent3.findElementAndClickFunction("yesButton");
+
+
+    }
+}
+
 
